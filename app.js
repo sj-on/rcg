8009913e54e5ffc6e8551aebdc5a96e8396ed746
DetPik <46680993+sj-on@users.noreply.github.com>
2022-01-29 22:22:27 +0530

    saafe

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const h1 = document.getElementById('trashgoeshere');
 
 note = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+accidental = ['#', '♭', '']
 thirdfifth = ['min', 'maj', 'dim', 'aug']
 seventh = ['maj7', 'min7']
 sussybois = ['sus2', 'sus4']
@@ -11,6 +12,7 @@ tf = thirdfifth[Math.floor(Math.random()*thirdfifth.length)]
 sev = seventh[Math.floor(Math.random()*seventh.length)]
 sus = sussybois[Math.floor(Math.random()*sussybois.length)]
 invert = inv[Math.floor(Math.random()*inv.length)]
+acc = accidental[Math.floor(Math.random()*accidental.length)]
 
 // console.log(n+tf+invert);
 coinflip = Math.floor(Math.random()*3);
@@ -18,14 +20,14 @@ coinflip = Math.floor(Math.random()*3);
 // console.log(coinflip);
 if(!coinflip){
     // seventh chord chosen
-    h1.innerHTML = `${n}<span>${tf}</span>${sev}<span>${invert}</span>`;
+    h1.innerHTML = `${n}${acc}<span>${tf}</span>${sev}<span>${invert}</span>`;
 }
 else if(coinflip == 1){
-    h1.innerHTML = `${n}<span>${tf}</span>${invert}`;
+    h1.innerHTML = `${n}${acc}<span>${tf}</span>${invert}`;
 }
 else{
     // a sus chord
-    h1.innerHTML = `${n}<span>${sus}</span>${invert}`;
+    h1.innerHTML = `${n}${acc}<span>${sus}</span>${invert}`;
 }
 
 // console.log(n+tf+sev);
